452c161d4a1fac4cf4a72246db7e8c5b3d39caba
MeliodasX <hamza.201001@gmail.com>
2025-06-15 04:08:50 +0500

    Part 2 - Going Beyond Rectangles

--- app/page.tsx
+++ app/page.tsx
@@ -1,44 +1,11 @@
 "use client";
 
-import { useEffect, useState } from "react";
-import { Canvas, Rect } from "fabric";
+import dynamic from "next/dynamic";
 
-export default function FabricVideoEditor() {
-  const [canvas, setCanvas] = useState<Canvas | null>(null);
+export default function home() {
+  const FabricVideoEditor = dynamic(() => import("@/components/fabric-video-editor"), {
+    ssr: false,
+  });
 
-  const addRectangle = () => {
-    const RGB = ["red", "green", "blue"];
-    const fill = RGB[Math.floor(Math.random() * 3)];
-    const rect = new Rect({
-      width: 100,
-      height: 100,
-      fill,
-    });
-
-    canvas?.add(rect);
-    canvas?.centerObject(rect);
-    canvas?.renderAll();
-  };
-
-  useEffect(() => {
-    const c = new Canvas("canvas", {
-      width: 960,
-      height: 540,
-      preserveObjectStacking: true,
-    });
-    setCanvas(c);
-
-    return () => {
-      c.dispose();
-    };
-  }, [setCanvas]);
-
-  return (
-    <>
-      <div className="h-[540px] w-[960px] border-2 border-black">
-        <canvas id="canvas" />
-      </div>
-      <button onClick={addRectangle}>Add RGB Block</button>
-    </>
-  );
+  return <FabricVideoEditor />;
 }
